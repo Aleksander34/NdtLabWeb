f0cfb25ff9f2f87ac6e0b75e5acb9da8086d1c56
Aleksander Litvinenko <aleksanderndt@gmail.com>
2022-11-13 19:09:21 +0300

    * filter

---
+++ js/jointsPage/index.js
@@ -0,0 +1,100 @@
+$(function () {
+	var _$modal = $('#exampleModal'),
+			_$table=$('#example');
+
+	_$table.DataTable({
+    scrollX: true,
+  });
+
+	_$modal
+		.on('shown.bs.modal', () => {}) //действия при открытии
+		.on('hidden.bs.modal', () => {
+			$('#requestFile').val('')
+			$('#requestPreviewInfo').addClass('d-none'); // действия при закрытии
+		}); //ивент который происходит во время запуска модального окна
+
+	$('#productionRequest').click(function () {
+		$('#qualificationRequestType').addClass('d-none');
+		$('#productionRequestType').removeClass('d-none');
+		$(this).addClass('active');
+		$('#qualificationRequest').removeClass('active');
+	});
+
+	$('#qualificationRequest').click(function () {
+		$('#productionRequestType').addClass('d-none');
+		$('#qualificationRequestType').removeClass('d-none');
+		$(this).addClass('active');
+		$('#productionRequest').removeClass('active');
+	});
+
+	$('.requestType').click(function () {
+		$('.requestType').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	$('#requestFile').change(function () {
+		if ($(this).get(0).files.length > 0) {
+			let fileName = $(this).get(0).files[0].name;
+			if (!fileName.match(/.*\.(xlsx|xls|xlsb)/)) {
+				toastr['warning']('Допустимые форматы: .xlsx .xls .xlsb', 'Не верный формат файла');
+				$('#requestPreviewInfo').addClass('d-none');
+				return;
+			}
+			$('#requestPreviewInfo').removeClass('d-none');
+		}
+	});
+
+	$('#requestPreviewTable').DataTable({
+		scrollX: true,
+	});
+
+	$('#saveRequest').click(function () {
+		$('#exampleModal').modal('hide');
+		swal('Успешно!', 'Заявка загружена', 'success');
+	});
+//фильтр таблицы по значениям колонки
+//получение всех значений в пределах колонки по номеру
+let colNum = 3;
+let column = _$table.DataTable().column(colNum-1);
+
+let data = column.data();
+console.log(data)
+
+//фильтрация значений колонки по введеному запросу в поиске
+let keyWord = "o";
+let filteredData = data.filter(x=>x.toLowerCase().includes(keyWord.toLowerCase()));
+console.log(filteredData);
+
+//программирование фильтра на странице
+$(".tableColumnFilter__items").text("");
+data.each(element => {
+	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
+</li>`)
+});
+
+$(".tableColumnFilter__search").on("keyup", function(){
+	let keyword = $(this).val().toLowerCase().trim();
+	console.log(keyword)
+	$(".tableColumnFilter__items").text("");
+	if(keyword=="")
+	{
+data.each(element => {
+	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
+</li>`)
+});
+return;
+	}
+	let filteredData = data.filter(x=>x.toLowerCase().includes(keyword.toLowerCase()));
+	filteredData.each(element => {
+		$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
+	</li>`)
+	});
+})
+
+$(".tableColumnFilter__filter-btn").click(function(){
+	let selectedItems = $(".tableColumnFilter__items input:checkbox:checked").siblings("span").map(function(){
+		return $(this).text();
+	});
+	console.log(selectedItems)
+})
+});
