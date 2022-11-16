3bc208c76f5b6a247297f0ed7f55f1d93d1a6100
Aleksander Litvinenko <aleksanderndt@gmail.com>
2022-11-16 18:05:59 +0300

    *filter

--- js/jointsPage/index.js
+++ js/jointsPage/index.js
@@ -1,16 +1,16 @@
-import initColumnFilter from "../libs/tableColumnFilter.js";
+import initColumnFilter from '../libs/tableColumnFilter.js';
 $(function () {
 	var _$modal = $('#exampleModal'),
-			_$table=$('#example');
+		_$table = $('#example');
 
 	_$table.DataTable({
-    scrollX: true,
-  });
+		scrollX: true,
+	});
 
 	_$modal
 		.on('shown.bs.modal', () => {}) //действия при открытии
 		.on('hidden.bs.modal', () => {
-			$('#requestFile').val('')
+			$('#requestFile').val('');
 			$('#requestPreviewInfo').addClass('d-none'); // действия при закрытии
 		}); //ивент который происходит во время запуска модального окна
 
@@ -53,78 +53,80 @@ $(function () {
 		$('#exampleModal').modal('hide');
 		swal('Успешно!', 'Заявка загружена', 'success');
 	});
-// //фильтр таблицы по значениям колонки
-// //получение всех значений в пределах колонки по номеру
-// let colNum = 3;
-// let column = _$table.DataTable().column(colNum-1);
+	// //фильтр таблицы по значениям колонки
+	// //получение всех значений в пределах колонки по номеру
+	// let colNum = 3;
+	// let column = _$table.DataTable().column(colNum-1);
 
-// let data = column.data();
-// console.log(data)
+	// let data = column.data();
+	// console.log(data)
 
-// //фильтрация значений колонки по введеному запросу в поиске
-// let keyWord = "o";
-// let filteredData = data.filter(x=>x.toLowerCase().includes(keyWord.toLowerCase()));
-// console.log(filteredData);
+	// //фильтрация значений колонки по введеному запросу в поиске
+	// let keyWord = "o";
+	// let filteredData = data.filter(x=>x.toLowerCase().includes(keyWord.toLowerCase()));
+	// console.log(filteredData);
 
-// //программирование фильтра на странице
-// $(".tableColumnFilter__items").text("");
-// data.each(element => {
-// 	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-// </li>`)
-// });
+	// //программирование фильтра на странице
+	// $(".tableColumnFilter__items").text("");
+	// data.each(element => {
+	// 	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
+	// </li>`)
+	// });
 
-// $(".tableColumnFilter__search").on("keyup", function(){
-// 	let keyword = $(this).val().toLowerCase().trim();
-// 	console.log(keyword)
-// 	$(".tableColumnFilter__items").text("");
-// 	if(keyword=="")
-// 	{
-// data.each(element => {
-// 	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-// </li>`)
-// });
-// return;
-// 	}
-// 	let filteredData = data.filter(x=>x.toLowerCase().includes(keyword.toLowerCase()));
-// 	filteredData.each(element => {
-// 		$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-// 	</li>`)
-// 	});
-// })
+	// $(".tableColumnFilter__search").on("keyup", function(){
+	// 	let keyword = $(this).val().toLowerCase().trim();
+	// 	console.log(keyword)
+	// 	$(".tableColumnFilter__items").text("");
+	// 	if(keyword=="")
+	// 	{
+	// data.each(element => {
+	// 	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
+	// </li>`)
+	// });
+	// return;
+	// 	}
+	// 	let filteredData = data.filter(x=>x.toLowerCase().includes(keyword.toLowerCase()));
+	// 	filteredData.each(element => {
+	// 		$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
+	// 	</li>`)
+	// 	});
+	// })
 
-// $(".tableColumnFilter__filter-btn").click(function(){
-// let selectedItems = [];
-// $(".tableColumnFilter__items input:checkbox:checked").siblings("span").each(function(){
-// 		selectedItems.push($(this).text());
-// 	});
+	// $(".tableColumnFilter__filter-btn").click(function(){
+	// let selectedItems = [];
+	// $(".tableColumnFilter__items input:checkbox:checked").siblings("span").each(function(){
+	// 		selectedItems.push($(this).text());
+	// 	});
 
-// let regExp ="";
-// if(selectedItems.length>0)
-// {
-// 	regExp="("+selectedItems.join("|");
-// 	regExp = regExp.substring(0, regExp.length-2);
-// 	regExp+=")";
-// 	console.log(regExp);
-// }
+	// let regExp ="";
+	// if(selectedItems.length>0)
+	// {
+	// 	regExp="("+selectedItems.join("|");
+	// 	regExp = regExp.substring(0, regExp.length-2);
+	// 	regExp+=")";
+	// 	console.log(regExp);
+	// }
 
-// 	let api = _$table.DataTable()
-// 	api
-// 	.column(colNum-1)
-// 	.search(regExp, true)
-// 	.draw();
+	// 	let api = _$table.DataTable()
+	// 	api
+	// 	.column(colNum-1)
+	// 	.search(regExp, true)
+	// 	.draw();
 
-// 	console.log(selectedItems)
-// })
+	// 	console.log(selectedItems)
+	// })
 
-// $(".tableColumnFilter__clear-btn").click(function(){
-// 	$(".tableColumnFilter__items input:checkbox:checked").each(function(){
-// 		$(this).prop("checked", false);
-// 		let api = _$table.DataTable()
-// 		api
-// 		.column(colNum-1)
-// 		.search("", true)
-// 		.draw();
-// 	});
-// })
-initColumnFilter("#example", ".tableColumnFilter")
+	// $(".tableColumnFilter__clear-btn").click(function(){
+	// 	$(".tableColumnFilter__items input:checkbox:checked").each(function(){
+	// 		$(this).prop("checked", false);
+	// 		let api = _$table.DataTable()
+	// 		api
+	// 		.column(colNum-1)
+	// 		.search("", true)
+	// 		.draw();
+	// 	});
+	// })
+	let f1 = initColumnFilter('#example', '.tableColumnFilter[data-num="3"]');
+	let f2 = initColumnFilter('#example', '.tableColumnFilter[data-num="2"]');
+	let f3 = initColumnFilter('#example', '.tableColumnFilter[data-num="1"]');
 });
--- js/libs/tableColumnFilter.js
+++ js/libs/tableColumnFilter.js
@@ -1,72 +1,80 @@
+export default function initColumnFilter(selectorTable, selectorFilter) {
+	let _$table = $(selectorTable);
+	let _$filter = $(selectorFilter);
 
+	let colNum = _$filter.data('num');
+	let column = _$table.DataTable().column(colNum - 1);
 
-// $(".tableColumnFilter__items").text("");
-// data.each(element => {
-// 	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-// </li>`)
-// });
+	let set = new Set();
+	column.data().each((element) => {
+		set.add(element);
+	});
 
-// $(".tableColumnFilter__search").on("keyup", function(){
-// 	let keyword = $(this).val().toLowerCase().trim();
-// 	console.log(keyword)
-// 	$(".tableColumnFilter__items").text("");
-// 	if(keyword=="")
-// 	{
-// data.each(element => {
-// 	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-// </li>`)
-// });
-// return;
-// 	}
-// 	let filteredData = data.filter(x=>x.toLowerCase().includes(keyword.toLowerCase()));
-// 	filteredData.each(element => {
-// 		$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-// 	</li>`)
-// 	});
-// })
+	let filter = {
+		data: [...set],
+		selectedItems: [],
+	};
 
-// $(".tableColumnFilter__filter-btn").click(function(){
-// let selectedItems = [];
-// $(".tableColumnFilter__items input:checkbox:checked").siblings("span").each(function(){
-// 		selectedItems.push($(this).text());
-// 	});
+	filter.data.forEach((element) => {
+		_$filter.find('.tableColumnFilter__items').append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
+</li>`);
+	});
 
-// let regExp ="";
-// if(selectedItems.length>0)
-// {
-// 	regExp="("+selectedItems.join("|");
-// 	regExp = regExp.substring(0, regExp.length-2);
-// 	regExp+=")";
-// 	console.log(regExp);
-// }
+	_$filter.find('.tableColumnFilter__search').on('keyup', function () {
+		let keyword = $(this).val().toLowerCase().trim();
+		_$filter.find('.tableColumnFilter__items').text('');
 
-// 	let api = _$table.DataTable()
-// 	api
-// 	.column(colNum-1)
-// 	.search(regExp, true)
-// 	.draw();
+		if (keyword == '') {
+			filter.data.forEach((element) => {
+				let isChecked = filter.selectedItems.includes(element);
+				_$filter.find('.tableColumnFilter__items').append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" ${
+					isChecked ? 'checked' : ''
+				} /><span>${element}</span></label>
+        </li>`);
+			});
+			return;
+		}
 
-// 	console.log(selectedItems)
-// })
+		let filteredData = filter.data.filter((x) => x.toLowerCase().includes(keyword.toLowerCase()));
+		filteredData.forEach((element) => {
+			_$filter.find('.tableColumnFilter__items').append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
+	</li>`);
+		});
+	});
 
-// $(".tableColumnFilter__clear-btn").click(function(){
-// 	$(".tableColumnFilter__items input:checkbox:checked").each(function(){
-// 		$(this).prop("checked", false);
-// 		let api = _$table.DataTable()
-// 		api
-// 		.column(colNum-1)
-// 		.search("", true)
-// 		.draw();
-// 	});
-// })
+	$(document).on('change', '.tableColumnFilter__items input:checkbox', function () {
+		let choisedElement = $(this).siblings('span').text();
+		console.log(choisedElement + ' ' + this.checked);
+		if (this.checked) {
+			filter.selectedItems.push(choisedElement);
+		} else {
+			filter.selectedItems = filter.selectedItems.filter((x) => x != choisedElement);
+		}
+	});
 
-export default function initColumnFilter(selectorTable, selectorFilter){
-let _$table = $(selectorTable);
-let _$filter = $(selectorFilter);
+	_$filter.find('.tableColumnFilter__filter-btn').click(function () {
+		let regExp = '';
+		if (filter.selectedItems.length > 0) {
+			regExp = '(' + filter.selectedItems.join('|');
+			regExp = regExp.substring(0, regExp.length - 2);
+			regExp += ')';
+		}
 
-let colNum = _$filter.data("num");
-let column = _$table.DataTable().column(colNum-1);
+		let api = _$table.DataTable();
+		api.column(colNum - 1)
+			.search(regExp, true)
+			.draw();
+	});
 
-let data = column.data();
-console.log(data)
+	_$filter.find('.tableColumnFilter__clear-btn').click(function () {
+		_$filter.find('.tableColumnFilter__items input:checkbox:checked').each(function () {
+			$(this).prop('checked', false);
+			let api = _$table.DataTable();
+			api.column(colNum - 1)
+				.search('', true)
+				.draw();
+		});
+	});
+
+	return filter;
 }
