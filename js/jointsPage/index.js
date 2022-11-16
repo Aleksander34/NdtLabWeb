772c42b006cc101c062239e2177abe811f3af61c
Aleksander Litvinenko <aleksanderndt@gmail.com>
2022-11-16 19:02:13 +0300

    *filter(double show)

--- js/jointsPage/index.js
+++ js/jointsPage/index.js
@@ -53,80 +53,8 @@ $(function () {
 		$('#exampleModal').modal('hide');
 		swal('Успешно!', 'Заявка загружена', 'success');
 	});
-	// //фильтр таблицы по значениям колонки
-	// //получение всех значений в пределах колонки по номеру
-	// let colNum = 3;
-	// let column = _$table.DataTable().column(colNum-1);
 
-	// let data = column.data();
-	// console.log(data)
-
-	// //фильтрация значений колонки по введеному запросу в поиске
-	// let keyWord = "o";
-	// let filteredData = data.filter(x=>x.toLowerCase().includes(keyWord.toLowerCase()));
-	// console.log(filteredData);
-
-	// //программирование фильтра на странице
-	// $(".tableColumnFilter__items").text("");
-	// data.each(element => {
-	// 	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-	// </li>`)
-	// });
-
-	// $(".tableColumnFilter__search").on("keyup", function(){
-	// 	let keyword = $(this).val().toLowerCase().trim();
-	// 	console.log(keyword)
-	// 	$(".tableColumnFilter__items").text("");
-	// 	if(keyword=="")
-	// 	{
-	// data.each(element => {
-	// 	$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-	// </li>`)
-	// });
-	// return;
-	// 	}
-	// 	let filteredData = data.filter(x=>x.toLowerCase().includes(keyword.toLowerCase()));
-	// 	filteredData.each(element => {
-	// 		$(".tableColumnFilter__items").append(`<li class="k-item"><label class="k-label k-checkbox-label"><input type="checkbox" class="k-checkbox k-checkbox-md k-rounded-md" value="Perth Pasties" /><span>${element}</span></label>
-	// 	</li>`)
-	// 	});
-	// })
-
-	// $(".tableColumnFilter__filter-btn").click(function(){
-	// let selectedItems = [];
-	// $(".tableColumnFilter__items input:checkbox:checked").siblings("span").each(function(){
-	// 		selectedItems.push($(this).text());
-	// 	});
-
-	// let regExp ="";
-	// if(selectedItems.length>0)
-	// {
-	// 	regExp="("+selectedItems.join("|");
-	// 	regExp = regExp.substring(0, regExp.length-2);
-	// 	regExp+=")";
-	// 	console.log(regExp);
-	// }
-
-	// 	let api = _$table.DataTable()
-	// 	api
-	// 	.column(colNum-1)
-	// 	.search(regExp, true)
-	// 	.draw();
-
-	// 	console.log(selectedItems)
-	// })
-
-	// $(".tableColumnFilter__clear-btn").click(function(){
-	// 	$(".tableColumnFilter__items input:checkbox:checked").each(function(){
-	// 		$(this).prop("checked", false);
-	// 		let api = _$table.DataTable()
-	// 		api
-	// 		.column(colNum-1)
-	// 		.search("", true)
-	// 		.draw();
-	// 	});
-	// })
-	let f1 = initColumnFilter('#example', '.tableColumnFilter[data-num="3"]');
-	let f2 = initColumnFilter('#example', '.tableColumnFilter[data-num="2"]');
-	let f3 = initColumnFilter('#example', '.tableColumnFilter[data-num="1"]');
+	let f1 = initColumnFilter('#example', '.colWithFilter[data-num="3"]');
+	let f2 = initColumnFilter('#example', '.colWithFilter[data-num="2"]');
+	let f3 = initColumnFilter('#example', '.colWithFilter[data-num="1"]');
 });
